eb827554aa72b7dfba92b2490a6d990c93efcc4b
Ahsan Ghazni Khan <ahsan123pk@gmail.com>
2022-10-26 09:37:33 +0500

    files and folders

---
+++ js/index.js
@@ -0,0 +1,43 @@
+var customers = [
+{
+  customerName: 'Yochai Benkler',
+  custtomerComments: 'Professor at Harvard Law School',
+  img_src: '',
+  customerDesination: ''
+},
+{
+    customerName: '',
+    custtomerComments: '',
+    img_src: '',
+    customerDesination: ''
+},
+{
+    customerName: '',
+    custtomerComments: '',
+    img_src: '',
+    customerDesination: ''
+},
+{
+    customerName: '',
+    custtomerComments: '',
+    img_src: '',
+    customerDesination: ''
+},
+{
+    customerName: '',
+    custtomerComments: '',
+    img_src: '',
+    customerDesination: ''
+},
+{
+    customerName: '',
+    custtomerComments: '',
+    img_src: '',
+    customerDesination: ''
+}
+]
+document.body.addEventListener('load', populateCustomers);
+
+function populateCustomers() {
+    
+}
